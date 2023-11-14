9a5e99e3c88a383953c41a297c0c63ea3614c70e
Kenny Nguyen <sokenns@gmail.com>
2023-11-13 22:03:36 -0800

    Add id to elements referenced in script

---
+++ script.js
@@ -0,0 +1,4 @@
+const password_container = document.querySelector("#password-container");
+const confirmation_container = document.querySelector("#confirmation-container");
+const password_input = document.querySelector("#password");
+const confirmation_input = document.querySelector("#confirm_password");
